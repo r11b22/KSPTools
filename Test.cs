2734e5fb1a1df24892f354ad348a99cef0c8bb43
r11b22 <145651956+JoostLehaen@users.noreply.github.com>
2024-07-27 14:26:02 +0200

    Start project

---
+++ Test.cs
@@ -0,0 +1,14 @@
+﻿using KSPTools;
+using UnityEngine;
+
+namespace KSPToolsTest
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Test : MonoBehaviour
+    {
+        void Start()
+        {
+            Tools.TestMessage("TestMessage");
+        }
+    }
+}
