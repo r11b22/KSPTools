50eb0002f1baf6519d80520e3d2b99cfcbd0124a
r11b22 <joost.lehaen@gmail.com>
2024-07-29 10:13:58 +0200

    Add usefull global parameters
    Getters:
    -GameFolderPath
    -SaveFolderName
    -SaveFolderPath
    -ActiveVessel
    -CurrentBody

---
+++ Globals.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+/*
+ * Requires UnityEngine
+ * Requires UnityEngine.CoreModule
+ */
+namespace KSPToolsTest
+{
+    public static class Globals
+    {
+        /// <summary>
+        /// returns the path of ksp folder
+        /// </summary>
+        /// <returns></returns>
+        public static string GetGameFolderPath()
+        {
+            return KSPUtil.ApplicationRootPath;
+        }
+        /// <summary>
+        /// returns only the folder name (not the full path)
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSaveFolderName() 
+        {
+            return HighLogic.SaveFolder;
+        }
+        /// <summary>
+        /// returns the full path to the current save folder
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSaveFolderPath() 
+        {
+            return KSPUtil.ApplicationRootPath + "/saves/" + HighLogic.SaveFolder;
+        }
+        /// <summary>
+        /// returns the active vessel
+        /// </summary>
+        /// <returns></returns>
+        public static Vessel GetActiveVessel() 
+        {
+            return FlightGlobals.ActiveVessel;
+        }
+        /// <summary>
+        /// returns the current celestial body
+        /// </summary>
+        /// <returns></returns>
+        public static CelestialBody GetCurrentBody() 
+        {
+            return FlightGlobals.currentMainBody;
+        }
+    }
+}
